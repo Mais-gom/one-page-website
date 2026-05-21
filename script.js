ecb0be3f2a4a26bc2ad5bafe486cb7b073e8e85d
Mais-gom <hlaielasayel@gmail.com>
2026-05-21 15:23:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,57 @@
-const galleryImage = document.querySelector(".gallery-img");
+const galleryImages = document.querySelectorAll(".gallery-img");
 const lightbox = document.getElementById("lightbox");
 const lightboxImg = document.getElementById("lightbox-img");
 const closeBtn = document.querySelector(".close");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
 
-galleryImage.addEventListener("click", function() {
-  lightbox.style.display = "block";
-  lightboxImg.src = galleryImage.src;
+let currentIndex = 0;
+
+const images = [
+  "images/image1.jpg",
+  "images/image2.jpg",
+  "images/image3.jpg"
+];
+
+galleryImages.forEach(function(image) {
+  image.addEventListener("click", function() {
+    currentIndex = Number(image.getAttribute("data-index"));
+    showImage();
+  });
 });
+
+function showImage() {
+  lightbox.style.display = "flex";
+  lightboxImg.src = images[currentIndex];
+}
+
+function nextImage() {
+  currentIndex++;
+
+  if (currentIndex >= images.length) {
+    currentIndex = 0;
+  }
+
+  showImage();
+}
+
+function previousImage() {
+  currentIndex--;
+
+  if (currentIndex < 0) {
+    currentIndex = images.length - 1;
+  }
+
+  showImage();
+}
 
 closeBtn.addEventListener("click", function() {
   lightbox.style.display = "none";
 });
+
+nextBtn.addEventListener("click", nextImage);
+
+prevBtn.addEventListener("click", previousImage);
 
 lightbox.addEventListener("click", function(event) {
   if (event.target === lightbox) {
